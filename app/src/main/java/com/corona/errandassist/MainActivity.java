38e68e72f763c18e822ac57d3a4e12619b53c204
Srinidhi Murthy <srinidhi.k.murthy@gmail.com>
2020-03-22 22:59:25 +0530

    Listeners

--- app/src/main/java/com/corona/errandassist/MainActivity.java
+++ app/src/main/java/com/corona/errandassist/MainActivity.java
@@ -4,19 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.corona.errandassist.signupEventListeners.SignupTextWatcher;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.corona.errandassist.MESSAGE";
+    private EditText userFName;
+    private EditText userLName;
+    private EditText userEmail;
+    private EditText userPhoneNumber;
+    private Button signUpButton;
+    private SignupTextWatcher signUpTextWatcher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getActivityFormObjects();
+
+        addTextWatchersForInputFields();
+    }
+
+    private void getActivityFormObjects() {
+        userFName = findViewById(R.id.fNameField);
+        userLName = findViewById(R.id.lNameField);
+        userEmail = findViewById(R.id.emailField);
+        userPhoneNumber = findViewById(R.id.phoneField);
+        signUpButton = findViewById(R.id.signupButton);
+    }
+
+    private void addTextWatchersForInputFields() {
+        signUpTextWatcher = new SignupTextWatcher(userFName, userLName, userEmail, userPhoneNumber, signUpButton);
+        userFName.addTextChangedListener(signUpTextWatcher);
+        userLName.addTextChangedListener(signUpTextWatcher);
+        userEmail.addTextChangedListener(signUpTextWatcher);
+        userPhoneNumber.addTextChangedListener(signUpTextWatcher);
     }
 
     public void onSignUpClicked(View view) {
+        openCompleteSignupActivity();
+    }
+
+
+
+    private void openCompleteSignupActivity() {
         Intent intent = new Intent(this, CompleteSignUpActivity.class);
         EditText editText = (EditText) findViewById(R.id.fNameField);
         String message = editText.getText().toString();
---
+++ app/src/main/java/com/corona/errandassist/signupEventListeners/SignupTextWatcher.java
@@ -0,0 +1,47 @@
+package com.corona.errandassist.signupEventListeners;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SignupTextWatcher implements TextWatcher {
+
+    private EditText userFName;
+    private EditText userLName;
+    private EditText userEmail;
+    private EditText userPhoneNumber;
+    private Button signUpButton;
+
+    public SignupTextWatcher(EditText userFName, EditText userLName, EditText userEmail, EditText userPhoneNumber, Button signUpButton){
+        this.userFName = userFName;
+        this.userLName = userLName;
+        this.userEmail = userEmail;
+        this.userPhoneNumber = userPhoneNumber;
+        this.signUpButton = signUpButton;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+        String fName = userFName.getText().toString();
+        String lName = userLName.getText().toString();
+        String emailID = userEmail.getText().toString();
+        String phoneNumber = userPhoneNumber.getText().toString();
+
+        boolean enableSignupButton = !(fName.isEmpty() || lName.isEmpty() || emailID.isEmpty() || phoneNumber.isEmpty());
+
+
+
+        signUpButton.setEnabled(enableSignupButton);
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
